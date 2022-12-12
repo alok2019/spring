5988cae486bbd1ad87dba857a4362ecee03b31ea
alok2019 <47350079+alok2019@users.noreply.github.com>
2022-12-12 23:54:16 +0530

    Update TestController.java

--- FirstSpringProject/src/main/java/com/java/TestController.java
+++ FirstSpringProject/src/main/java/com/java/TestController.java
@@ -2,7 +2,7 @@ package com.java;
 
 public class TestController {
   
-  public void getProducts(String a){
+  public void getProducts(int a, int b){
     System.out.println("getAllProducts");
   }
 
